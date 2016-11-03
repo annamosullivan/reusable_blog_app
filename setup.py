a11a8636a1e4fde67611413e72083322789f3254
Anne <osullivan_anne@hotmail.com>
2016-11-03 20:54:29 +0000

    removed + signs from edit post button

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='reusable-blog-app',
-    version='1.0.4',
+    version='1.0.5',
     packages=['reusable_blog'],
     include_package_data=True,
     license='BSD License',  # example license
