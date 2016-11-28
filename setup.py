bb115ac41bb06c3dd4d6f1eac7e31cce03a03d4f
Anne <osullivan_anne@hotmail.com>
2016-11-28 22:43:10 +0000

    incremented version in setup.py,

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='reusable-blog-app',
-    version='1.0.12',
+    version='1.0.13',
     packages=['reusable_blog'],
     include_package_data=True,
     license='BSD License',  # example license
