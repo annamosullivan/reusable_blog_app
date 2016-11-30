5830ab8ebe17069a0f2005da738b196b7d7bb5b8
Anne <osullivan_anne@hotmail.com>
2016-11-30 22:55:17 +0000

    incremented version no in line 12 and got rid of django-disqus

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='reusable-blog-app',
-    version='1.0.14',
+    version='1.0.15',
     packages=['reusable_blog'],
     include_package_data=True,
     license='BSD License',  # example license
@@ -34,6 +34,5 @@ setup(
         'django_forms_bootstrap',
         'Disqus',
         'django',
-        'django-disqus',
     ],
 )
